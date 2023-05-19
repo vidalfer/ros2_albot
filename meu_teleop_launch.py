b002525467b1c5d11107ece0a9aceec294ee3bf1
vidalfer <80919616+vidalfer@users.noreply.github.com>
2023-05-19 12:34:16 -0300

    Update meu_teleop_launch.py

--- meu_teleop_launch.py
+++ meu_teleop_launch.py
@@ -4,8 +4,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='teleop_control',
-            executable='teleop_node',
+            package='meu_teleop_control',
+            executable='meu_teleop_node',
             output='screen',
             parameters=[{'linear_speed': 0.5, 'angular_speed': 0.5, 'speed_increment': 0.5}],
         ),
