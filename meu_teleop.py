495166993e54ce90b3bc46163017bc25b357f583
vidalfer <80919616+vidalfer@users.noreply.github.com>
2023-05-19 12:32:08 -0300

    Update meu_teleop.py

--- meu_teleop.py
+++ meu_teleop.py
@@ -3,17 +3,19 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-class TeleopNode(Node):
+# Definindo a nossa classe TeleopNode, que herda de Node
+class MeuTeleopNode(Node):
 
     def __init__(self):
+        # Chamando o construtor da classe pai
         super().__init__('teleop_node')
 
-        # configurando os parametros
+        # Declarando e inicializando os parâmetros do nó
         self.linear_speed = self.declare_parameter('linear_speed', 0.5).value
         self.angular_speed = self.declare_parameter('angular_speed', 0.5).value
         self.speed_increment = self.declare_parameter('speed_increment', 0.5).value
 
-        # configurando o subscriber
+        # Criando o subscriber para o tópico do teclado
         self.subscription = self.create_subscription(
             String,
             'keyboard_topic',
@@ -22,15 +24,17 @@ class TeleopNode(Node):
         )
         self.subscription
 
-        # configurando o publisher
+        # Criando o publisher para o tópico de comando de velocidade do robô
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
 
+    # Função de callback que será chamada sempre que uma nova mensagem chegar no tópico do teclado
     def listener_callback(self, msg):
         self.get_logger().info(f'Recebido: {msg.data}')
 
+        # Inicializando a mensagem Twist que será enviada ao robô
         twist = Twist()
 
-        #incremento de velocidade ao apertar f
+        # Verificando qual tecla foi pressionada e alterando as velocidades ou publicando a mensagem correspondente
         if msg.data.lower() == 'f':
             self.linear_speed += self.speed_increment
             self.angular_speed += self.speed_increment
@@ -56,19 +60,26 @@ class TeleopNode(Node):
             self.publisher.publish(twist)
 
 
+# Função principal que será chamada quando o script for executado
 def main(args=None):
+    # Inicializando o ROS
     rclpy.init(args=args)
 
-    teleop_node = TeleopNode()
+    # Criando a nossa instância do TeleopNode
+    meu_teleop_node = MeuTeleopNode()
 
     try:
-        rclpy.spin(teleop_node)
+        # Entrando no loop de execução do ROS. Isso fará com que as callbacks sejam chamadas até que o nó seja desligado.
+        rclpy.spin(meu_teleop_node)
     except KeyboardInterrupt:
+        # Se o nó for interrompido por um sinal de interrupção (como CTRL+C), ele sairá do loop de execução
         pass
 
-    teleop_node.destroy_node()
+    # Destruindo o nó e desligando o ROS
+    meu_teleop_node.destroy_node()
     rclpy.shutdown()
 
 
+# Se o script for executado diretamente (em vez de importado), chamamos a função principal
 if __name__ == '__main__':
     main()
